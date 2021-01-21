101dbe9154e7e06b46881c3cb3c67aecc5c0ca81
mmcristiano <maiamoraiscristiano@gmail.com>
2021-01-20 21:03:22 -0300

    Add Handler interface

--- Todo.Domain/Commands/Contracts/ICommand.cs
+++ Todo.Domain/Commands/Contracts/ICommand.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Flunt.Validations;
+﻿using Flunt.Validations;
+using System;
 
 namespace Todo.Domain.Commands.Contracts
 {
--- Todo.Domain/Commands/MarkTodoAsDoneCommand.cs
+++ Todo.Domain/Commands/MarkTodoAsDoneCommand.cs
@@ -1,4 +1,5 @@
 ﻿using Flunt.Notifications;
+using Flunt.Validations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
