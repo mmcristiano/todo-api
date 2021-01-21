33f3a3467a277ad85035ac9a997ade3819d58ebb
mmcristiano <maiamoraiscristiano@gmail.com>
2021-01-20 22:44:52 -0300

    Add CommandTests

---
+++ Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Todo.Domain.Commands;
+
+namespace Todo.Domain.Tests.CommandTests
+{
+    [TestClass]
+    public class CreateTodoCommandTests
+    {
+        [TestMethod]
+        public void Dado_um_comando_invalido()
+        {
+            var command = new CreateTodoCommand("", "", DateTime.Now);
+            command.Validate();
+
+            Assert.AreEqual(command.Valid, false);
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido()
+        {
+            var command = new CreateTodoCommand("Titulo", "cristianomorais", DateTime.Now);
+            command.Validate();
+
+            Assert.AreEqual(command.Valid, true);
+        }
+
+    }
+}
