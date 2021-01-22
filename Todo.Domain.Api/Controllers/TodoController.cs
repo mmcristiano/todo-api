b7b4f8c3819e02180ea69de1c9f703592551bf3e
mmcristiano <maiamoraiscristiano@gmail.com>
2021-01-21 23:27:36 -0300

    Todo Api methods

---
+++ Todo.Domain.Api/Controllers/TodoController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todo.Domain.Commands;
+using Todo.Domain.Entities;
+using Todo.Domain.Handlers;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/todos")]
+    public class TodoController : ControllerBase
+    {
+        [Route("")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            string user = "cristianomorais";
+            return repository.GetAll(user);
+        }
+
+        [Route("done")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllDone(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetAllDone("cristianomorais");
+        }
+
+        [Route("undone")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllUndone(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetAllUndone("cristianomorais");
+        }
+
+        [Route("done/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDoneForToday(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetByPeriod("cristianomorais", DateTime.Now.Date, true);
+        }
+
+        [Route("undone/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetUndoneForToday(
+           [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetByPeriod("cristianomorais", DateTime.Now.Date, false);
+        }
+
+        [Route("done/tomorrow")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDoneForTomorrow(
+           [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetByPeriod("cristianomorais", DateTime.Now.Date.AddDays(1), true);
+        }
+
+
+        [Route("undone/tomorrow")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetUndoneForTomorrow(
+           [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetByPeriod("cristianomorais", DateTime.Now.Date.AddDays(1), false);
+        }
+
+
+        [Route("")]
+        [HttpPost]
+        public GenericCommandResult Create(
+            [FromBody] CreateTodoCommand command,
+            [FromServices] TodoHandler handler
+        )
+        {
+            command.User = "cristianomorais";
+            return (GenericCommandResult) handler.Handle(command);
+        }
+
+        [Route("")]
+        [HttpPut]
+        public GenericCommandResult Update(
+            [FromBody] UpdateTodoCommand command,
+            [FromServices] TodoHandler handler)
+        {
+            command.User = "cristianomorais";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("mark-as-done")]
+        [HttpPut]
+        public GenericCommandResult MarkAsDone (
+            [FromBody] MarkTodoAsDoneCommand command,
+            [FromServices] TodoHandler handler)
+        {
+            command.User = "cristianomorais";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("mark-as-undone")]
+        [HttpPut]
+        public GenericCommandResult MarkAsUndone(
+            [FromBody] MarkTodoAsUndoneCommand command,
+            [FromServices] TodoHandler handler)
+        {
+            command.User = "cristianomorais";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+    }
+}
